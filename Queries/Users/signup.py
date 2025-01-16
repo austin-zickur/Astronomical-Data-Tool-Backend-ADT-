c06be80f5c962f0fcb2e4392636c8a26a505719a
Kylee <kyleebrown.work@gmail.com>
2025-01-16 14:38:02 -0600

    updated signup.py

--- Queries/Users/signup.py
+++ Queries/Users/signup.py
@@ -1,2 +1,39 @@
-# FIXME 
-# from supabase import create_client, Client
+from supabase import initialize_supabase
+
+'''
+FileName: signup
+
+Developer: Kylee B
+
+Description: functions to query supabase for user authentication
+
+Imported into: 
+    * SignUp.py
+
+Functions:
+    * signUp():
+        input:
+            email (String)
+            password (String)
+        output:
+            data (JSON)
+
+'''
+
+# initialize supabase for use
+supabase = initialize_supabase()
+
+def signUp(email, password):
+    try:
+        data = supabase.auth.sign_up({
+            'email': f'{email}',
+            'password': f'{password}',
+            'options': {
+                'email_redirect_to': 'https://example.com/welcome',
+                },
+        })
+        print(data)
+        return(data)
+    except Exception as e:
+        print(f"error connecting to DB -- {e}")
+        return(f"error connecting to DB -- {e}")
