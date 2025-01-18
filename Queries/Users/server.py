f3b0e3c5dd640d5d1e0ab2a9392610fef2a42bd7
Kylee <kyleebrown.work@gmail.com>
2025-01-17 20:01:55 -0600

    files for A

---
+++ Queries/Users/server.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from SignUp import user_bp
+
+
+# Initialize Flask app
+app = Flask(__name__)
+
+
+# Register blueprints
+#app.register_blueprint(user_bp, url_prefix='/adt-api')
+app.register_blueprint(user_bp)
+#app.register_blueprint(post_bp, url_prefix='/api')
+
+# Run the app
+if __name__ == '__main__':
+    app.run(port=8000, debug=True)
